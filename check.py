c7692fd96ab5cd43a02ed36b8534f6e23100e116
Bikash <bkash.timsina@gmail.com>
2024-10-04 21:01:09 +0545

    explore playwright

--- check.py
+++ check.py
@@ -1,10 +1,19 @@
-from playwright.sync_api import sync_playwright
-from time import sleep
+import asyncio
+from playwright.async_api import async_playwright, Playwright
+import time
 
+url = 'https://www.amazon.com/s?k=gaming+headsets&_encoding=UTF8&content-id=amzn1.sym.12129333-2117-4490-9c17-6d31baf0582a&pd_rd_r=3978412d-3c67-4c01-83f0-f244be38ab2b&pd_rd_w=RlFz5&pd_rd_wg=m0OhB&pf_rd_p=12129333-2117-4490-9c17-6d31baf0582a&pf_rd_r=4XFYV3PJFVZZHK4JWM4Q&ref=pd_hp_d_atf_unk'
+async def run(playwright: Playwright):
+    chromium = playwright.chromium # or "firefox" or "webkit".
+    browser = await chromium.launch(headless=False)
+    page = await browser.new_page()
+    await page.goto(url)
+    time.sleep(5)
 
-with sync_playwright() as playwright:
-    browser = playwright.chromium.launch(headless=False, slow_mo=500)
-    page = browser.new_page()
-    page.goto("https://geonode.com/free-proxy-list")
-    while True:
-        proxies
+    # other actions...
+    await browser.close()
+
+async def main():
+    async with async_playwright() as playwright:
+        await run(playwright)
+asyncio.run(main())
